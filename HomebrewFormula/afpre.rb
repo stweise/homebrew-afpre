47d9d18cdd0061e96565874c0697005b3a982feb
Steffen Weise <steffen.weise@mercateo.com>
2019-01-14 21:08:20 +0100

    replacing chars

--- HomebrewFormula/afpre.rb
+++ HomebrewFormula/afpre.rb
@@ -1,16 +1,16 @@
 class Afpre < Formula
-  homepage 'https://github.com/leflamm/afpre'
-  url 'https://github.com/leflamm/afpre.git', :tag => '0.9.19'
+	homepage 'https://github.com/leflamm/afpre'
+	url 'https://github.com/leflamm/afpre.git', :tag => '0.9.19'
 
-  devel do
-    url 'https://github.com/leflamm/afpre.git', :branch => 'master'
-  end
+	devel do
+		url 'https://github.com/leflamm/afpre.git', :branch => 'master'
+	end
 
-  def install
-    bin.install 'afpre'
-  end
+	def install
+		bin.install 'afpre'
+	end
 
-  test do
-    system "#{HOMEBREW_PREFIX}/bin/afpre", "--version"
-  end
+	test do
+		system "#{HOMEBREW_PREFIX}/bin/afpre", "--version"
+	end
 end
