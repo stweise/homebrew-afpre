62f446702cbc0c7ad323f1ecdf2cabd29594a40c
Steffen Weise <steffen.weise@mercateo.com>
2019-01-14 20:56:35 +0100

    adding basic formula

---
+++ HomebrewFormula/afpre.rb
@@ -0,0 +1,16 @@
+class Ethereum < Formula
+  homepage 'https://github.com/leflamm/afpre'
+  url 'https://github.com/leflamm/afpre.git', :tag => '0.9.19'
+
+  devel do
+    url 'https://github.com/leflamm/afpre.git', :branch => 'master'
+  end
+
+  def install
+    bin.install 'afpre'
+  end
+
+  test do
+    system "#{HOMEBREW_PREFIX}/bin/afpre", "--version"
+  end
+end
