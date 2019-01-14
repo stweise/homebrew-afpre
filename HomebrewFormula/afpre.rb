0e88eaab2d90326151d1a9227367150da83a9849
Steffen Weise <stweise@users.noreply.github.com>
2019-01-14 21:59:22 +0100

    Update for dependency + long description

--- HomebrewFormula/afpre.rb
+++ HomebrewFormula/afpre.rb
@@ -1,6 +1,8 @@
 class Afpre < Formula
+  desc "CLI for the AWS Federation Proxy found at https://github.com/ImmobilienScout24/afp-cli"
   homepage "https://github.com/leflamm/afpre"
   url "https://github.com/leflamm/afpre.git", :tag => "0.9.19"
+  depends_on "jq"
   def install
     bin.install "afpre"
   end
