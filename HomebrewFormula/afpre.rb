13931dd4c0bf3d52710790c0d8343762cfe1c8b6
Steffen Weise <steffen.weise@mercateo.com>
2019-01-14 21:13:39 +0100

    exchanging quotes

--- HomebrewFormula/afpre.rb
+++ HomebrewFormula/afpre.rb
@@ -1,16 +1,10 @@
 class Afpre < Formula
-	homepage 'https://github.com/leflamm/afpre'
-	url 'https://github.com/leflamm/afpre.git', :tag => '0.9.19'
-
-	devel do
-		url 'https://github.com/leflamm/afpre.git', :branch => 'master'
-	end
-
-	def install
-		bin.install 'afpre'
-	end
-
-	test do
-		system "#{HOMEBREW_PREFIX}/bin/afpre", "--version"
-	end
+  homepage "https://github.com/leflamm/afpre"
+  url "https://github.com/leflamm/afpre.git", :tag => "0.9.19"
+  def install
+    bin.install "afpre"
+  end
+  test do
+    system "#{HOMEBREW_PREFIX}/bin/afpre", "--version"
+  end
 end
